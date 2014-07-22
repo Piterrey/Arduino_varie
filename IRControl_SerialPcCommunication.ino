0c031ebe0c28389806756ec4cecf8f17730c517c
Piterrey <sapartangiu@libero.it>
2014-07-23 00:29:27 +0200

    Update IRControl_SerialPcCommunication.ino

--- IRControl_SerialPcCommunication.ino
+++ IRControl_SerialPcCommunication.ino
@@ -8,7 +8,7 @@ IRrecv irrecv(11);
 decode_results results;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   irrecv.enableIRIn(); // Start the receiver
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
@@ -24,13 +24,13 @@ void loop() {
         //test if the pc sent an 'a' or 'b'
         switch (commandIn)
         {
-            case 'a':
+            case '1':
             {
                 //we got an 'a' from the pc so turn on the digital pin
                 digitalWrite(DIGI_PIN_SOMETHING,HIGH);
                 break;
             }
-            case 'b':
+            case '0':
             {
                 //we got an 'b' from the pc so turn off the digital pin
                 digitalWrite(DIGI_PIN_SOMETHING,LOW);
@@ -38,7 +38,7 @@ void loop() {
             }
         }
     }
-  /*------------------------------------------------- if con filtro IR -------------------------------------------------*/
+  /*------------------------------------------------- if con filtro ---------------------------------------------------*/
   if (irrecv.decode(&results)) {
     //Serial.println(results.value);
     switch(results.value){
@@ -48,34 +48,66 @@ void loop() {
                {
                  digitalWrite(2,HIGH);
                  on=1;
-                 Serial.println("key 1 pressed"); 
+                 Serial.println("1"); 
                  delay(10);
                }
                else
                {
                  digitalWrite(2,LOW);
                  on=0;
-                 Serial.println("key 1 pressed"); 
+                 Serial.println("1"); 
                  delay(10);
                }
          break;
+         
     /*---------------------------------------------- tasto 2 ------------------------------------------*/
      case 3674004067:
              if(on==0)
                {
                  digitalWrite(3,HIGH);
                  on=1;
-                 Serial.println("key 2 pressed"); 
+                 Serial.println("2"); 
                  delay(10);
                }
                else
                {
                  digitalWrite(3,LOW);
                  on=0;
-                 Serial.println("key 2 pressed"); 
+                 Serial.println("2"); 
                  delay(10);
                }
          break;
+         
+    /*---------------------------------------------- tasto destra ------------------------------------------*/
+     case 1313865889:
+                 Serial.println("Right"); 
+         break;
+         
+    /*---------------------------------------------- tasto destra ------------------------------------------*/
+     case 3607392081:
+                 Serial.println("Up"); 
+         break;
+         
+    /*---------------------------------------------- tasto destra ------------------------------------------*/
+     case 3379257582:
+                 Serial.println("Down"); 
+         break;   
+         
+    /*---------------------------------------------- tasto destra ------------------------------------------*/
+     case 643505126:
+                 Serial.println("Left"); 
+         break;
+         
+    /*------------------------------------------------ tasto ok --------------------------------------------*/
+     case 848474797:
+                 Serial.println("Click"); 
+         break;
+         
+    /*---------------------------------------------- tasto on/off ------------------------------------------*/     
+     case 2152446381:
+                 Serial.println("Off"); 
+         break;
+         
     /*---------------------------------------------- default -------------------------------------------*/
      default:
          break;
@@ -84,3 +116,4 @@ void loop() {
   }
   
 }
+
